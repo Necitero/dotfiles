b0f4e1bcbe0d498fcd6f710b2fd396b288e243ec
Necitero <necitero@gmail.com>
2025-11-10 21:19:05 +0100

    feat(nvim): add hidden files to telescope

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -1,8 +1,12 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 local opts = { noremap = true, silent = true }
 
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>x', ':bd<CR>', opts)
-vim.keymap.set('n', '<leader>l', ':bprevious<CR>', opts)
-vim.keymap.set('n', '<leader>h', ':bnext<CR>', opts)
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>ff", function()
+	builtin.find_files({
+		find_command = { "rg", "--ignore", "--hidden", "--files", "--glob", "!**/.git/**" },
+	})
+end, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>x", ":bd<CR>", opts)
+vim.keymap.set("n", "<leader>l", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<leader>h", ":bnext<CR>", opts)
