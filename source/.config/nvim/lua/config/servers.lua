09fa3439ed35ebe3db10b4d87af4001cd514422b
Necitero <necitero@gmail.com>
2025-11-27 11:00:20 +0100

    feat(nvim): add more lsp servers

--- source/.config/nvim/lua/config/servers.lua
+++ source/.config/nvim/lua/config/servers.lua
@@ -1,2 +1,17 @@
-local servers = { 'lua_ls', 'ts_ls', 'jsonls', 'tailwindcss', 'yamlls' }
+local servers = {
+	"cssls",
+	"css_variables",
+	"cssmodules_ls",
+	"jsonls",
+	"lua_ls",
+	"markdown_oxide",
+	"tailwindcss",
+	"ts_ls",
+	"yamlls",
+	"lua_ls",
+	"ts_ls",
+	"jsonls",
+	"tailwindcss",
+	"yamlls",
+}
 return servers
