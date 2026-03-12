31aa5facd7d3d62df5b035e68f98253fd3e70d0a
Necitero <necitero@gmail.com>
2026-03-12 10:03:48 +0100

    feat(nvim): add clear search and remove duplicate servers

--- source/.config/nvim/lua/config/servers.lua
+++ source/.config/nvim/lua/config/servers.lua
@@ -2,17 +2,14 @@ local servers = {
 	"cssls",
 	"css_variables",
 	"cssmodules_ls",
-	"jsonls",
-	"lua_ls",
 	"markdown_oxide",
-	"tailwindcss",
 	"ts_ls",
 	"yamlls",
 	"lua_ls",
-	"ts_ls",
 	"jsonls",
 	"tailwindcss",
 	"yamlls",
 	"html",
+	"bashls",
 }
 return servers
