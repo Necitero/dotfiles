f11a3e47e7a3eee6fb675ccb51e282d549a861d3
Necitero <34024540+Necitero@users.noreply.github.com>
2026-04-06 14:32:46 +0200

    refactor(nvim): remove colorizer, shorten gh path

--- source/.config/nvim/lua/plugins/init.lua
+++ source/.config/nvim/lua/plugins/init.lua
@@ -1,23 +1,23 @@
+local gh = "https://github.com/"
 vim.pack.add({
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-	{ src = "https://github.com/folke/snacks.nvim" },
-	{ src = "https://github.com/nvim-lualine/lualine.nvim", name = "lualine" },
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	{ src = "https://github.com/mason-org/mason.nvim", name = "mason" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim", name = "mason-lspconfig" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
-	{ src = "https://github.com/hrsh7th/cmp-buffer" },
-	{ src = "https://github.com/hrsh7th/cmp-path" },
-	{ src = "https://github.com/hrsh7th/cmp-cmdline" },
-	{ src = "https://github.com/hrsh7th/nvim-cmp", name = "nvim-cmp" },
-	{ src = "https://github.com/willothy/nvim-cokeline", name = "cokeline" },
-	{ src = "https://github.com/stevearc/conform.nvim" },
-	{ src = "https://github.com/f-person/git-blame.nvim", name = "gitblame" },
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	{ src = "https://github.com/mistricky/codesnap.nvim" },
-	{ src = "https://github.com/norcalli/nvim-colorizer.lua" },
+	{ src = gh .. "nvim-lua/plenary.nvim" },
+	{ src = gh .. "catppuccin/nvim", name = "catppuccin" },
+	{ src = gh .. "folke/snacks.nvim" },
+	{ src = gh .. "nvim-lualine/lualine.nvim", name = "lualine" },
+	{ src = gh .. "nvim-tree/nvim-web-devicons" },
+	{ src = gh .. "mason-org/mason.nvim", name = "mason" },
+	{ src = gh .. "mason-org/mason-lspconfig.nvim", name = "mason-lspconfig" },
+	{ src = gh .. "neovim/nvim-lspconfig" },
+	{ src = gh .. "hrsh7th/cmp-nvim-lsp" },
+	{ src = gh .. "hrsh7th/cmp-buffer" },
+	{ src = gh .. "hrsh7th/cmp-path" },
+	{ src = gh .. "hrsh7th/cmp-cmdline" },
+	{ src = gh .. "hrsh7th/nvim-cmp", name = "nvim-cmp" },
+	{ src = gh .. "willothy/nvim-cokeline", name = "cokeline" },
+	{ src = gh .. "stevearc/conform.nvim" },
+	{ src = gh .. "f-person/git-blame.nvim", name = "gitblame" },
+	{ src = gh .. "lewis6991/gitsigns.nvim" },
+	{ src = gh .. "mistricky/codesnap.nvim" },
 })
 
 require("plugins/mason")
@@ -28,4 +28,3 @@ require("plugins/conform")
 require("plugins/lualine")
 require("plugins/git-blame")
 require("plugins/codesnap")
-require("colorizer").setup()
