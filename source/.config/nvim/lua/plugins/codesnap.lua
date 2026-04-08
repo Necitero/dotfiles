61df16240103b33c629df1fc4637ecba504d244e
Necitero <necitero@gmail.com>
2026-04-08 10:30:05 +0200

    feat(nvim): add catppuccin codesnap and treesitter-manager

--- source/.config/nvim/lua/plugins/codesnap.lua
+++ source/.config/nvim/lua/plugins/codesnap.lua
@@ -1,69 +1,72 @@
 require("codesnap").setup({
-	show_line_number = true,
-	highlight_color = "#ffffff20",
-	show_workspace = true,
-	snapshot_config = {
-		theme = "vercel@https://raw.githubusercontent.com/Railly/one-hunter-vscode/refs/heads/main/themes/OneHunter-Vercel-color-theme.json",
-		window = {
-			mac_window_bar = false,
-			shadow = {
-				radius = 20,
-				color = "#00000040",
-			},
-			margin = {
-				x = 32,
-				y = 32,
-			},
-			border = {
-				width = 1,
-				color = "#ffffff30",
-			},
-			title_config = {
-				color = "#ffffff",
-				font_family = "Pacifico",
-			},
-		},
-		line_number_color = "#495162",
-		command_output_config = {
-			prompt = "❯",
-			font_family = "NotoSansM Nerd Font Mono",
-			prompt_color = "#F78FB3",
-			command_color = "#98C379",
-			string_arg_color = "#ff0000",
-		},
-		code_config = {
-			font_family = "NotoSansM Nerd Font Mono",
-			breadcrumbs = {
-				enable = true,
-				separator = "/",
-				color = "#80848b",
-				font_family = "NotoSansM Nerd Font Mono",
-			},
-		},
-		watermark = {
-			content = "",
-			font_family = "Pacifico",
-			color = "#ffffff",
-		},
-		background = {
-			start = {
-				x = 0,
-				y = 0,
-			},
-			["end"] = {
-				x = "max",
-				y = 0,
-			},
-			stops = {
-				{
-					position = 0,
-					color = "#303446",
-				},
-				{
-					position = 1,
-					color = "#292c3c",
-				},
-			},
-		},
-	},
+    show_line_number = true,
+    highlight_color = "#ffffff20",
+    show_workspace = true,
+    snapshot_config = {
+        theme = "Catppuccin-Frappe",
+        themes_folders = {
+            "~/.config/codesnap/themes",
+        },
+        window = {
+            mac_window_bar = false,
+            shadow = {
+                radius = 20,
+                color = "#00000040",
+            },
+            margin = {
+                x = 32,
+                y = 32,
+            },
+            border = {
+                width = 1,
+                color = "#ffffff30",
+            },
+            title_config = {
+                color = "#ffffff",
+                font_family = "Pacifico",
+            },
+        },
+        line_number_color = "#495162",
+        command_output_config = {
+            prompt = "❯",
+            font_family = "NotoSansM Nerd Font Mono",
+            prompt_color = "#F78FB3",
+            command_color = "#98C379",
+            string_arg_color = "#ff0000",
+        },
+        code_config = {
+            font_family = "NotoSansM Nerd Font Mono",
+            breadcrumbs = {
+                enable = true,
+                separator = "/",
+                color = "#80848b",
+                font_family = "NotoSansM Nerd Font Mono",
+            },
+        },
+        watermark = {
+            content = "",
+            font_family = "Pacifico",
+            color = "#ffffff",
+        },
+        background = {
+            start = {
+                x = 0,
+                y = 0,
+            },
+            ["end"] = {
+                x = "max",
+                y = 0,
+            },
+            stops = {
+                {
+                    position = 0,
+                    color = "#303446",
+                },
+                {
+                    position = 1,
+                    color = "#292c3c",
+                },
+            },
+        },
+    },
 })
--- source/.config/nvim/lua/plugins/init.lua
+++ source/.config/nvim/lua/plugins/init.lua
@@ -1,23 +1,24 @@
 local gh = "https://github.com/"
 vim.pack.add({
-	{ src = gh .. "nvim-lua/plenary.nvim" },
-	{ src = gh .. "catppuccin/nvim", name = "catppuccin" },
-	{ src = gh .. "folke/snacks.nvim" },
-	{ src = gh .. "nvim-lualine/lualine.nvim", name = "lualine" },
-	{ src = gh .. "nvim-tree/nvim-web-devicons" },
-	{ src = gh .. "mason-org/mason.nvim", name = "mason" },
-	{ src = gh .. "mason-org/mason-lspconfig.nvim", name = "mason-lspconfig" },
-	{ src = gh .. "neovim/nvim-lspconfig" },
-	{ src = gh .. "hrsh7th/cmp-nvim-lsp" },
-	{ src = gh .. "hrsh7th/cmp-buffer" },
-	{ src = gh .. "hrsh7th/cmp-path" },
-	{ src = gh .. "hrsh7th/cmp-cmdline" },
-	{ src = gh .. "hrsh7th/nvim-cmp", name = "nvim-cmp" },
-	{ src = gh .. "willothy/nvim-cokeline", name = "cokeline" },
-	{ src = gh .. "stevearc/conform.nvim" },
-	{ src = gh .. "f-person/git-blame.nvim", name = "gitblame" },
-	{ src = gh .. "lewis6991/gitsigns.nvim" },
-	{ src = gh .. "mistricky/codesnap.nvim" },
+    { src = gh .. "romus204/tree-sitter-manager.nvim" },
+    { src = gh .. "nvim-lua/plenary.nvim" },
+    { src = gh .. "catppuccin/nvim",                  name = "catppuccin" },
+    { src = gh .. "folke/snacks.nvim" },
+    { src = gh .. "nvim-lualine/lualine.nvim",        name = "lualine" },
+    { src = gh .. "nvim-tree/nvim-web-devicons" },
+    { src = gh .. "mason-org/mason.nvim",             name = "mason" },
+    { src = gh .. "mason-org/mason-lspconfig.nvim",   name = "mason-lspconfig" },
+    { src = gh .. "neovim/nvim-lspconfig" },
+    { src = gh .. "hrsh7th/cmp-nvim-lsp" },
+    { src = gh .. "hrsh7th/cmp-buffer" },
+    { src = gh .. "hrsh7th/cmp-path" },
+    { src = gh .. "hrsh7th/cmp-cmdline" },
+    { src = gh .. "hrsh7th/nvim-cmp",                 name = "nvim-cmp" },
+    { src = gh .. "willothy/nvim-cokeline",           name = "cokeline" },
+    { src = gh .. "stevearc/conform.nvim" },
+    { src = gh .. "f-person/git-blame.nvim",          name = "gitblame" },
+    { src = gh .. "lewis6991/gitsigns.nvim" },
+    { src = gh .. "mistricky/codesnap.nvim" },
 })
 
 require("plugins/mason")
@@ -28,3 +29,12 @@ require("plugins/conform")
 require("plugins/lualine")
 require("plugins/git-blame")
 require("plugins/codesnap")
+require("tree-sitter-manager").setup({
+    ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "json",
+        "bash",
+    },
+})
